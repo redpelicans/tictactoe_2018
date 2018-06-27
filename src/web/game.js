3dc40a26db204ea985e3f72f069dd01d97699a53
eric-basley <eric.basley@redpelicans.com>
2018-06-27 14:16:48 +0200

    updates

--- src/web/game.js
+++ src/web/game.js
@@ -1,8 +1,16 @@
+import { times, update } from 'ramda';
+
 export const GAME_OVER = 'GAME_OVER';
 export const GAME_RUNNING = 'GAME_RUNNING';
 export const X = 'x';
 export const O = 'o';
 export const isStatusOver = status => status === GAME_OVER;
-const isEmptyCell = cell => !cell;
+export const isEmptyCell = cell => !cell;
 export const isBoardFull = board => !board.some(isEmptyCell);
 export const getStatus = board => (isBoardFull(board) ? GAME_OVER : GAME_RUNNING);
+export const getNewBoard = () => times(() => null, 9);
+export const getNewPlayer = ({ player, computer }) => (Math.round(Math.random()) ? player : computer);
+export const isComputer = currentPlayer => currentPlayer && currentPlayer.isComputer;
+export const switchPlayer = ({ currentPlayer, player, computer }) => (isComputer(currentPlayer) ? player : computer);
+export const getNextBoard = ({ currentPlayer: { piece }, board }, cell) => update(cell, piece, board);
+export const computerPlay = board => board.indexOf(null);
--- src/web/index.js
+++ src/web/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { render } from 'react-dom';
 import App from './components/App';
-import { X, O, GAME_OVER } from './game';
+import { getNewBoard, GAME_OVER, O, X } from './game';
 
 const { pathname } = document.location;
 const name = pathname.slice(1) || 'Unknown player';
-const player = { name };
-const computer = { name: 'computer', isComputer: true };
-const board = [X, O, X, null, null, X, O, null, null];
-const history = [{ id: 1, winner: player }, { id: 2, winner: computer }, { id: 3, winner: player }, { id: 4 }];
+const player = { name, piece: O };
+const computer = { name: 'computer', isComputer: true, piece: X };
+const history = [];
+const board = getNewBoard();
 
 const state = {
-  currentPlayer: player,
   status: GAME_OVER,
   player,
   computer,
-  board,
   history,
+  board,
 };
 
 console.log('mounting React ...'); // eslint-disable-line no-console
